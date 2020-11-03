ebbf86583701f8bba5f8c7c2577a3dacec2ce260
retasu0141 <47290741+retasu0141@users.noreply.github.com>
2020-11-04 06:32:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class Entry(db.Model):
     __tablename__ = "db"
 
     # カラムを定義
-    ID_ = db.Column(db.String(), nullable=False, primary_key=True)
+    _ = db.Column(db.String(), nullable=False, primary_key=True)
     speaker = db.Column(db.String(), nullable=False, primary_key=True)
     pitch = db.Column(db.String(), nullable=False, primary_key=True)
     speed = db.Column(db.String(), nullable=False, primary_key=True)
