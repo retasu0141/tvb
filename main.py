001355ada091ecff6a1816b5e11b2a0debd489b1
retasu0141 <47290741+retasu0141@users.noreply.github.com>
2020-11-04 01:12:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import os
 bot = commands.Bot(command_prefix="!")
 token = os.environ['DISCORD_BOT_TOKEN']
 
+if not discord.opus.is_loaded(): 
+    #もし未ロードだったら
+    discord.opus.load_opus("heroku-buildpack-libopus")
 
 def voicetext(text,speaker_number):
 	#print(speaker_number)
